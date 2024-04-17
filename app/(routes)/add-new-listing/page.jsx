303f020932a2aa5c5b2e299f1844ae511fe6ac6c
mohsensami <mohsensami18727@gmail.com>
2024-04-17 23:07:35 +0330

    Edit listing and upload images

--- app/(routes)/add-new-listing/page.jsx
+++ app/(routes)/add-new-listing/page.jsx
@@ -84,7 +84,7 @@ export default function AddNewListing() {
             setLoader(false);
             console.log('New Data added,', data);
             toast('New Address added for listing');
-            // router.replace('/edit-listing/' + data[0].id);
+            router.replace('/edit-listing/' + data[0].id);
         }
         if (error) {
             setLoader(false);
@@ -94,26 +94,36 @@ export default function AddNewListing() {
     };
 
     return (
-        <div className="container">
-            <div className=" mt-40 flex items-center gap-2">
-                <AsyncSelect
-                    className="w-full"
-                    // cacheOptions={options.map((item) => {
-                    //     return {
-                    //         value: item.formatted,
-                    //         label: item.formatted,
-                    //     };
-                    // })}
-                    // defaultOptions
-                    loadOptions={fetchCoordinates}
-                    onChange={handleChange}
-                    placeholder="Search for a post..."
-                    isClearable={true}
-                />
-                {/* <Button onClick={nextHandler}>Submit</Button> */}
-                <Button disabled={options.length == 0 || loader} onClick={nextHandler}>
-                    {loader ? <Loader className="animate-spin" /> : 'Next'}
-                </Button>
+        <div className="mt-10 md:mx-56 lg:mx-80">
+            <div
+                className="p-10 flex flex-col
+    gap-5 items-center justify-center"
+            >
+                <h2 className="font-bold text-3xl">Add New Listing</h2>
+                <div
+                    className="p-10  rounded-lg border
+        w-full
+        shadow-md flex flex-col gap-5"
+                >
+                    <h2 className="text-gray-500 text-lg">Enter Address which you want to list</h2>
+                    <AsyncSelect
+                        className="w-full"
+                        // cacheOptions={options.map((item) => {
+                        //     return {
+                        //         value: item.formatted,
+                        //         label: item.formatted,
+                        //     };
+                        // })}
+                        // defaultOptions
+                        loadOptions={fetchCoordinates}
+                        onChange={handleChange}
+                        placeholder="Search for a post..."
+                        isClearable={true}
+                    />
+                    <Button disabled={!options?.label || loader} onClick={nextHandler}>
+                        {loader ? <Loader className="animate-spin" /> : 'Next'}
+                    </Button>
+                </div>
             </div>
         </div>
     );
